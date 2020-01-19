b4421cefddbef78eb43f8b6c1357279d6f87af0e
Tyler-R <trubenui@sfu.ca>
2020-01-18 17:15:49 -0800

    Use PORT environment variable to configure port.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
 
+let PORT = process.env.PORT || 8000
+
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(3000)
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
